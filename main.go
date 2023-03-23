b818020a2794f6329724417b2cfff6d4da07f0a6
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2023-03-23 19:47:03 -0400

    update-automated-save

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"k8s.io/api/core/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
@@ -19,6 +20,7 @@ import (
 )
 
 // go get k8s.io/client-go@v0.26.3
+var timeout int64 = 60
 
 func main() {
 	kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
@@ -128,9 +130,48 @@ func deletePod(clientset *kubernetes.Clientset, namespace, podName string) error
 		return fmt.Errorf("failed to delete pod %s in namespace %s: %v", podName, namespace, err)
 	}
 
+	err = waitForPodDeletion(clientset, namespace, podName, &timeout)
+	if err != nil {
+		return fmt.Errorf("failed to wait for pod %s in namespace %s to be deleted: %v", podName, namespace, err)
+	}
+
 	return nil
 }
 
+func waitForPodDeletion(clientset *kubernetes.Clientset, namespace, podName string, timeout *int64) error {
+
+	var watchOptions = metav1.ListOptions{
+		FieldSelector:  fmt.Sprintf("metadata.name=%s", podName),
+		TimeoutSeconds: timeout,
+		Watch:          true,
+	}
+	watcher, err := clientset.CoreV1().Pods(namespace).Watch(context.Background(), watchOptions)
+	if err != nil {
+		return err
+	}
+
+	defer watcher.Stop()
+
+	fmt.Println("Waiting for pod deletion...")
+	ch := watcher.ResultChan()
+	for {
+		select {
+		case event, ok := <-ch:
+			if !ok {
+				return fmt.Errorf("watch channel closed")
+			}
+
+			switch event.Type {
+			case watch.Deleted:
+				return nil
+			case watch.Error:
+				return fmt.Errorf("watch error: %v", event.Object)
+			}
+		case <-time.After(time.Duration(*timeout) * time.Second):
+			return fmt.Errorf("timeout waiting for pod deletion")
+		}
+	}
+}
 func execCommandsInPod(clientset *kubernetes.Clientset, namespace, podName, containerName string, commands []string, outputFile string) error {
 	var outputBuffer bytes.Buffer
 
